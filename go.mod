34258f4023903872b877ade45a92edf1d1926fc3
Muhamad Danil Rafiqi <danil.rafiqi@gmail.com>
2019-12-20 16:47:53 +0700

    more dinamic

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/danilrafiqi/gin-swagger
 go 1.13
 
 require (
-	github.com/gin-contrib/sessions v0.0.1 // indirect
+	github.com/gin-contrib/sessions v0.0.1
 	github.com/gin-gonic/gin v1.5.0
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/json-iterator/go v1.1.8 // indirect
--- swagger.go
+++ swagger.go
@@ -1,6 +1,7 @@
 package ginSwagger
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 	"path"
@@ -10,61 +11,95 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Config struct {
+	Route          *gin.RouterGroup
+	User           map[string]string
+	Url            string
+	Docs           string
+	Authentication bool
+}
+
 type swaggerUIBundle struct {
 	URL         string
 	DeepLinking bool
 	SwaggerUrl  string
 }
 
-func checkAuth(swaggerUrl string) gin.HandlerFunc {
+func authenticate(conf Config) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		session := sessions.Default(c)
+		fmt.Println("login status", session.Get("login"))
 		if session.Get("login") == true {
 			c.Next()
 		} else {
 			session.Clear()
-			c.Redirect(http.StatusMovedPermanently, swaggerUrl+"/login")
+			session.Save()
+			c.Redirect(http.StatusMovedPermanently, conf.Url+"/login")
 			c.Next()
 		}
 	}
 }
 
-func InitAuth(r *gin.RouterGroup, user map[string]string, swaggerUrl string) {
-	var loginPath = path.Join("views", "login.html")
-	var login, _ = template.ParseFiles(loginPath)
-	r.Static("/assets", "./assets")
-	r.StaticFile("/swagger.json", "./docs/swagger.json")
-	r.GET("/login", func(c *gin.Context) {
+func login(conf Config) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var loginPath = path.Join("views", "login.html")
+		var login, _ = template.ParseFiles(loginPath)
 		login.Execute(c.Writer, swaggerUIBundle{
-			URL: swaggerUrl,
+			URL: conf.Url,
 		})
-	})
+	}
+}
 
-	store := cookie.NewStore([]byte("secret"))
-	r.Use(sessions.Sessions("mysession", store))
-
-	r.POST("/login", func(c *gin.Context) {
+func loginProccess(conf Config) gin.HandlerFunc {
+	return func(c *gin.Context) {
 		username := c.PostForm("username")
 		password := c.PostForm("password")
 		session := sessions.Default(c)
 
-		for key, val := range user {
+		for key, val := range conf.User {
 			if key == username && val == password {
 				session.Set("login", true)
 				session.Save()
-				c.Redirect(http.StatusMovedPermanently, swaggerUrl)
+				c.Redirect(http.StatusMovedPermanently, conf.Url)
 			} else {
 				session.Clear()
-				c.Redirect(http.StatusMovedPermanently, swaggerUrl+"/login")
+				session.Save()
+				c.Redirect(http.StatusMovedPermanently, conf.Url+"/login")
 			}
 		}
+	}
+}
 
-	})
-	var indexPath = path.Join("views", "index.html")
-	var index, _ = template.ParseFiles(indexPath)
-	r.GET("/", checkAuth(swaggerUrl), func(c *gin.Context) {
+func logout(conf Config) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		session.Clear()
+		session.Save()
+		c.Redirect(http.StatusMovedPermanently, conf.Url+"/login")
+	}
+}
+
+func index(conf Config) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var indexPath = path.Join("views", "index.html")
+		var index, _ = template.ParseFiles(indexPath)
 		index.Execute(c.Writer, swaggerUIBundle{
-			URL: swaggerUrl + "/swagger.json",
+			URL: conf.Url + conf.Docs,
 		})
-	})
+	}
+}
+
+func Init(config Config) {
+	config.Route.Static("/assets", "./assets")
+	config.Route.StaticFile(config.Docs, "."+config.Docs)
+	store := cookie.NewStore([]byte("secret"))
+	config.Route.Use(sessions.Sessions("mysession", store))
+	config.Route.GET("/login", login(config))
+	config.Route.POST("/login", loginProccess(config))
+	config.Route.GET("/logout", logout(config))
+	if config.Authentication == true {
+		config.Route.GET("/", authenticate(config), index(config))
+	} else {
+		config.Route.GET("/", index(config))
+	}
 }
