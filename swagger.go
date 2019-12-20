bb3fe89733fd074c02ca3f0e387d178fb474d45d
Muhamad Danil Rafiqi <danil.rafiqi@gmail.com>
2019-12-20 13:48:46 +0700

    add asset and view

--- swagger.go
+++ swagger.go
@@ -2,8 +2,11 @@ package ginSwagger
 
 import (
 	"html/template"
+	"net/http"
 	"path"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,13 +16,55 @@ type swaggerUIBundle struct {
 	SwaggerUrl  string
 }
 
-func InitAuth(r *gin.RouterGroup, user map[string]string) {
+func checkAuth(swaggerUrl string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		if session.Get("login") == true {
+			c.Next()
+		} else {
+			session.Clear()
+			c.Redirect(http.StatusMovedPermanently, swaggerUrl+"/login")
+			c.Next()
+		}
+	}
+}
+
+func InitAuth(r *gin.RouterGroup, user map[string]string, swaggerUrl string) {
+	var loginPath = path.Join("views", "login.html")
+	var login, _ = template.ParseFiles(loginPath)
+	r.Static("/assets", "./assets")
+	r.StaticFile("/swagger.json", "./docs/swagger.json")
+	r.GET("/login", func(c *gin.Context) {
+		login.Execute(c.Writer, swaggerUIBundle{
+			URL: swaggerUrl,
+		})
+	})
+
+	store := cookie.NewStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
+
+	r.POST("/login", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+		session := sessions.Default(c)
+
+		for key, val := range user {
+			if key == username && val == password {
+				session.Set("login", true)
+				session.Save()
+				c.Redirect(http.StatusMovedPermanently, swaggerUrl)
+			} else {
+				session.Clear()
+				c.Redirect(http.StatusMovedPermanently, swaggerUrl+"/login")
+			}
+		}
+
+	})
 	var indexPath = path.Join("views", "index.html")
 	var index, _ = template.ParseFiles(indexPath)
-	r.GET("/", func(c *gin.Context) {
-		index.Execute(c.Writer, &swaggerUIBundle{
-			URL:         "http://localhost:8200/docs.json",
-			DeepLinking: true,
+	r.GET("/", checkAuth(swaggerUrl), func(c *gin.Context) {
+		index.Execute(c.Writer, swaggerUIBundle{
+			URL: swaggerUrl + "/swagger.json",
 		})
 	})
 }
